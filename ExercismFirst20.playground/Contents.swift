7b55e992eab6b3e63c06f8a3b96cac3cb08281e8
Onur Altıntaş <onur.altintas2045@gmail.com>
2026-01-25 00:16:44 +0300

    Algorithm 7: Magician-in-Training, SOLVED.

--- ExercismFirst20.playground/Contents.swift
+++ ExercismFirst20.playground/Contents.swift
@@ -248,3 +248,39 @@ print("\n--------------------------------------------------------\n")
 // Solution
 
 print("Algorithm 7: Magician-in-Training \n")
+
+func getCard(at index: Int, from stack: [Int]) -> Int {
+    return stack[index]
+}
+
+func setCard(at index: Int, in stack: [Int], to newCard: Int) -> [Int] {
+    guard index < stack.count && index >= 0 else { return stack }
+    
+    var newStack = stack
+    newStack[index] = newCard
+    return newStack
+}
+
+func insert(_ newCard: Int, atTopOf stack: [Int]) -> [Int] {
+    var newStack = stack
+    newStack.append(newCard)
+    return newStack
+}
+
+func removeCard(at index: Int, from stack: [Int]) -> [Int] {
+    guard index < stack.count && index >= 0 else { return stack }
+    var newStack = stack
+    newStack.remove(at: index)
+    return newStack
+}
+
+func insert(_ newCard: Int, at index: Int, from stack: [Int]) -> [Int] {
+    guard index <= stack.count && index >= 0 else { return stack }
+    var newStack = stack
+    newStack.insert(newCard, at: index)
+    return newStack
+}
+
+func checkSizeOfStack(_ stack: [Int], _ size: Int) -> Bool {
+    stack.count == size
+}
