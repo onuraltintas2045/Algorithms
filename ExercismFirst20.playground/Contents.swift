d05b7e3a52241a812cf061ca5416589e46caf49e
Onur Altıntaş <onur.altintas2045@gmail.com>
2026-01-25 00:54:53 +0300

    Algorithm 8: ChessBoard, TESTED.

--- ExercismFirst20.playground/Contents.swift
+++ ExercismFirst20.playground/Contents.swift
@@ -314,3 +314,22 @@ func getRow(_ board : [String], rank: Int) -> [String] {
     
     return Array(board[rowStartPosition...rowEndPosition])
 }
+
+checkPrint("isValidSquare", isValidSquare(rank: 1, file: "A"), expected: true)
+checkPrint("isValidSquare", isValidSquare(rank: 9, file: "A"), expected: false)
+checkPrint("isValidSquare", isValidSquare(rank: 1, file: "Z"), expected: false)
+
+let board = [
+  "1A", "1B", "1C", "1D", "1E", "1F", "1G", "1H",
+  "2A", "2B", "2C", "2D", "2E", "2F", "2G", "2H",
+  "3A", "3B", "3C", "3D", "3E", "3F", "3G", "3H",
+  "4A", "4B", "4C", "4D", "4E", "4F", "4G", "4H",
+  "5A", "5B", "5C", "5D", "5E", "5F", "5G", "5H",
+  "6A", "6B", "6C", "6D", "6E", "6F", "6G", "6H",
+  "7A", "7B", "7C", "7D", "7E", "7F", "7G", "7H",
+  "8A", "8B", "8C", "8D", "8E", "8F", "8G", "8H"
+]
+
+checkPrint("GetRow: 1", getRow(board, rank: 1), expected: ["1A", "1B", "1C", "1D", "1E", "1F", "1G", "1H"])
+checkPrint("GetRow: 3", getRow(board, rank: 3), expected: ["3A", "3B", "3C", "3D", "3E", "3F", "3G", "3H"])
+
