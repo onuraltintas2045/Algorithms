ba8f6cdadaf090e7ee890cbd0c2319a651b9b5c2
Onur Altıntaş <onur.altintas2045@gmail.com>
2026-01-24 23:46:36 +0300

    Algorithm 6: Vexing Vehicle Purchase, SOLVED.

--- ExercismFirst20.playground/Contents.swift
+++ ExercismFirst20.playground/Contents.swift
@@ -189,3 +189,37 @@ print("\n--------------------------------------------------------\n")
 
 print("Algorithm 6: Vexing Vehicle Purchase \n")
 
+func canIBuy(vehicle: String, price: Double, monthlyBudget: Double) -> String {
+    let fiveYearBudget = monthlyBudget * 60
+    
+    if price <= fiveYearBudget {
+        return "Yes! I'm getting a \(vehicle)"
+    } else if (price - fiveYearBudget) <= (fiveYearBudget * 0.1) {
+        return "I'll have to be frugal if I want a \(vehicle)"
+    } else {
+        return "Darn! No \(vehicle) for me"
+    }
+}
+
+func licenseType(numberOfWheels wheels: Int) -> String {
+    switch wheels{
+        case 2,3:
+            return "You will need a motorcycle license for your vehicle"
+        case 4,6:
+            return "You will need an automobile license for your vehicle"
+        case 18:
+            return "You will need a commercial trucking license for your vehicle"
+        default:
+            return "We do not issue licenses for those types of vehicles"
+    }
+}
+
+func calculateResellPrice(originalPrice: Int, yearsOld: Int) -> Int {
+    if yearsOld < 3 {
+        return (originalPrice * 80) / 100
+    } else if yearsOld >= 10 {
+        return (originalPrice * 50) / 100
+    } else {
+        return (originalPrice * 70) / 100
+    }
+}
